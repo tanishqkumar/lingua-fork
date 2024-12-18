5d19a50e82c8726a8be217724f5e448aaaf61f5f
Neil Band <20114038+nband@users.noreply.github.com>
2024-12-18 12:08:27 -0800

    add utils for converting hf checkpoint to dcp format

---
+++ apps/cpt/cpt_ckpt_utils.py
@@ -0,0 +1,11 @@
+LLAMA_PRETRAINED_CKPT_NAMES = {
+    "meta-llama/Llama-3.2-1B",
+    "meta-llama/Llama-3.2-3B",
+    "meta-llama/Llama-3.1-8B",
+    "meta-llama/Llama-3.1-70B",
+    "meta-llama/Llama-3.1-405B",
+}
+
+
+def is_llama_pretrained_ckpt(ckpt_path: str) -> bool:
+    return any(ckpt_path.endswith(name) for name in LLAMA_PRETRAINED_CKPT_NAMES)
